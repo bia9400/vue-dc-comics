8b0b2e1664a27ca0316b89736e6aaf2f5a32f2c5
bia9400 <ferro.biagio@gmail.com>
2022-06-21 16:53:24 +0200

    Finish

--- src/components/SocialEndingr.vue
+++ src/components/SocialEndingr.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <div class="mybutton">ciao</div>
+  <div class="row secondFooter py-3">
+    <div class="col ms-5 d-flex align-items-center" >
+      <button class="my-btn ">SIGN-UP NOW!</button>
     </div>
     <div class="col">
-      <div class="d-flex">
+      <div class="d-flex justify-content-center align-items-center">
         <h3>FOLLOW US</h3>
         <img src="../assets/img/footer-facebook.png" alt="" />
         <img src="../assets/img/footer-periscope.png" alt="" />
@@ -22,4 +22,20 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.secondFooter{
+    background-color: #303030;
+    h3{
+        color: #027eec;
+    }
+    img{
+        padding: 0 1rem;
+    }
+}
+.my-btn{
+    border: 2px solid #027eec;
+    background-color: #303030;
+    padding: .5rem;
+    color: white;
+}
+</style>
